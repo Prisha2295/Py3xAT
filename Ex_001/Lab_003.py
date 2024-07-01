d5a095a289462044be29bbc5ddc8ac40a2507360
Prisha2295 <prishayadav22@gmail.com>
2024-07-01 22:52:04 +0530

    Adding File Lab 002

---
+++ Ex_001/Lab_003.py
@@ -0,0 +1,9 @@
+import math
+radius = float(input("Enter the radius of the circle: "))
+Area = math.pi * math.pow(radius,2)
+Area2 = math.pi * (radius * 2)
+
+# print("The Area of circle is : ", Area2)
+
+b = 5
+print("Factorial of b is : ", math.factorial(b))
